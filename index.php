3408bff771cd2bbfb225f17862d06aaa4daed939
aledic85 <dicuollo85@gmail.com>
2019-04-17 11:21:21 +0200

    add "include"

--- index.php
+++ index.php
@@ -24,19 +24,24 @@
      <title></title>
    </head>
    <body>
-
      <?php
 
       function randomImg() {
 
-        $imgs = ["https://www.faige.it/wp-content/uploads/2018/03/berlin-la-casa-de-papel-4.jpg.png", "https://o.aolcdn.com/images/dims3/GLOB/crop/720x361+0+23/resize/630x315!/format/jpg/quality/85/http%3A%2F%2Fo.aolcdn.com%2Fhss%2Fstorage%2Fmidas%2Fb55798773a41af6d294e478b55b82774%2F206290751%2Fcasa-di-carta.jpg", "https://www.repstatic.it/content/nazionale/img/2018/04/18/145829307-48f09ea6-af53-42b5-8aac-74461a4b58e5.jpg", "https://www.tvzoom.it/wp-content/uploads/2018/04/la-casa-de-papel.jpg", "https://images2.corriereobjects.it/methode_image/2018/07/28/Interni/Foto%20Interni%20-%20Trattate/casa-de-papel-profesor-k3CI--620x349@abc-kb9E-U30001255616255QXH-593x443@Corriere-Web-Sezioni.jpg?v=20180728223718"];
+        include "vars.php";
 
         $imgsLength = count($imgs);
-        $rndNum = rand(1, $imgsLength-1);
+        $rndNum = rand(0, $imgsLength-1);
 
-        $img = $imgs[$rndNum];
+        do {
 
-        echo "<img src='$img'>";
+          $img = $imgs[$rndNum];
+          $path = $img["path"];
+          $url = $img["url"];
+
+        } while ($img["isActive"] == false);    
+
+        echo "<a href='$url'><img src='$path'></a>";
       }
 
       randomImg();
